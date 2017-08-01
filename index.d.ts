3ec77002f498a43a06e38caaa4f5ccfabe39dcb5
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-08-01 21:50:49 +0200

    Fix TypeScript declaration file

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,13 @@
-export interface IGCFilenameData {
-    callsign: string | null;
-    date: string;
-    manufacturer: string | null;
-    loggerId: string | null;
-    numFlight: number | null;
+declare namespace parse {
+    export interface IGCFilenameData {
+        callsign: string | null;
+        date: string;
+        manufacturer: string | null;
+        loggerId: string | null;
+        numFlight: number | null;
+    }
 }
 
-declare function parse(filename: string, maxYear?: number): IGCFilenameData | null;
+declare function parse(filename: string, maxYear?: number): parse.IGCFilenameData | null;
 
 export = parse;
